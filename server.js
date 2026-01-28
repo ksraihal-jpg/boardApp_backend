7dd24079c9e0846fa9bec23c859404bc164e387e
Kartik <ksraihal@gmail.com>
2026-01-28 18:40:53 +0530

    changes cors url

--- server.js
+++ server.js
@@ -29,8 +29,9 @@ connectToDB();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:3000"], 
+      origin: process.env.FRONTEND_URL || "http://localhost:3000", 
       methods: ["GET", "POST"],
+      credentials: true,
     },
   });
 
